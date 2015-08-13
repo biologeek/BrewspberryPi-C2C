74a9e4b2ea44703cdd45577ed6d5fa158ddeb473
xavier <xavier@xavier-CX61-2PC>
2015-08-13 23:00:00 +0200

    init

---
+++ TemperatureDisplay/WebContent/js/servletCall.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+	
+	function callGraphServlet (){
+
+		var formDelay = 2000;
+		console.log(document.getElementById("formDelay").value);
+
+		var img = document.getElementById("JFreeGraphServlet");
+		
+		console.log(img.id);
+		$.ajax({
+			url: "JFreeGraphServlet", 
+			type: "GET",
+			success: function( notification ) {
+				console.log("JFreeGraphServlet?r="+Math.random());
+
+				img.parentNode.removeChild(img);
+				var imgGraph = document.createElement("img");
+				imgGraph.id ="JFreeGraphServlet";
+				document.getElementById("servletGraph").appendChild(imgGraph);
+				imgGraph.src = "JFreeGraphServlet";
+			},
+			error: function(data){
+				//handle any error 
+			}
+		});
+		
+	}
+	
+	function callTemperatureServlet (){
+		
+		var txt = document.getElementById("probeTemperaturesTab");
+		$.ajax({
+			url: "TemperatureServlet", 
+			type: "GET",
+			success: function( notification ) {
+				console.log("TemperatureServlet?r="+Math.random());
+
+				txt.parentNode.removeChild(txt);
+				var txt = document.createElement("table");
+				txt.id= "probeTemperaturesTab";
+				document.getElementById("probeTemperatures").appendChild(txt);
+			},
+			error: function(data){
+				console.log("Error : "+data); 
+			}
+		});
+	}
+	
+	function callServlet () {
+		callTemperatureServlet();
+		callGraphServlet();
+	}	
+	
+	setInterval(callServlet,1000);
+
+});
