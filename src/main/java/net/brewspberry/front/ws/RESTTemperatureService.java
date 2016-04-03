3d7d5d18f9d4b34597bace707c61fab81802a7ab
biologeek <bob57050@hotmail.fr>
2016-04-03 23:21:27 +0200

    added rest webservice for text temperature

--- src/main/java/net/brewspberry/front/ws/RESTTemperatureService.java
+++ src/main/java/net/brewspberry/front/ws/RESTTemperatureService.java
@@ -2,6 +2,8 @@ package net.brewspberry.front.ws;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
@@ -33,13 +35,16 @@ public class RESTTemperatureService {
 	ISpecificTemperatureMeasurementService tmesSpecService = new TemperatureMeasurementServiceImpl();
 	IGenericService<Etape> stepService = new EtapeServiceImpl();
 	private Etape currentStep;
-	
-	Logger logger = LogManager.getInstance(RESTTemperatureService.class.getName());
+
+	Logger logger = LogManager.getInstance(RESTTemperatureService.class
+			.getName());
+
 	@GET
-	@Path("/initTemperatures/e/{e}/u/{u}")
+	@Path("/initTemperatures/e/{e}/u/{u}/limitTo/{limitTo}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response initTemperatureForStep(@PathParam("u") String uuid,
-			@PathParam("e") long stepID) throws JSONException {
+			@PathParam("e") long stepID, @PathParam("limitTo") int limit)
+			throws JSONException {
 
 		JSONObject json = new JSONObject();
 		JSONArray result = null;
@@ -52,10 +57,11 @@ public class RESTTemperatureService {
 
 			List<TemperatureMeasurement> tmesList = tmesSpecService
 					.getTemperatureMeasurementByEtape(currentStep);
+			logger.info("List size : "+tmesList.size());
 
 			if (uuid != null) {
 
-				if (uuid == "all") {
+				if (!uuid.equals("all")) {
 					Iterator<TemperatureMeasurement> it = tmesList.iterator();
 
 					while (it.hasNext()) {
@@ -65,22 +71,23 @@ public class RESTTemperatureService {
 						}
 
 					}
-					result = this.convertListToJSONObject(filteredList);
-
-				} else {
-					result = this.convertListToJSONObject(tmesList);
+					tmesList = filteredList;
 				}
-			} else {
-				result = this.convertListToJSONObject(tmesList);
+
 			}
 
-		}
+			logger.info("List size : "+tmesList.size());
 
+			while (tmesList.size() > limit) {
+				
+				logger.info("List size : "+tmesList.size());
+				tmesList.remove(0);
+			}
+			result = this.convertListToJSONObject(tmesList);
+		}
 		return Response.status(200).entity(result.toString()).build();
 	}
 
-	
-	
 	@GET
 	@Path("/initTemperatures/e/{e}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -90,11 +97,11 @@ public class RESTTemperatureService {
 	 * @return
 	 * @throws JSONException
 	 */
-	public Response initTemperature(@PathParam("e") long stepID) throws JSONException {
+	public Response initTemperature(@PathParam("e") long stepID)
+			throws JSONException {
 
 		JSONObject json = new JSONObject();
 		JSONArray result = null;
-
 
 		if (stepID > 0) {
 
@@ -105,13 +112,12 @@ public class RESTTemperatureService {
 
 			result = this.convertListToJSONObject(tmesList);
 
-
 		}
 
 		return Response.status(200).entity(result.toString()).build();
-		
-		
+
 	}
+
 	@GET
 	@Path("/updateTemperatures/e/{e}/u/{u}/l/{l}/d/{d}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -125,55 +131,64 @@ public class RESTTemperatureService {
 	 * @return
 	 */
 	public Response updateTemperatureForStep(@PathParam("e") long stepID,
-			@PathParam("u") String uuid, @PathParam("l") long lastID, @PathParam("d") int delayInMinutes) throws JSONException {
+			@PathParam("u") String uuid, @PathParam("l") long lastID,
+			@PathParam("d") int delayInMinutes) throws JSONException {
 
 		List<TemperatureMeasurement> result = new ArrayList<TemperatureMeasurement>();
 		JSONArray jsonResult = null;
-		if (lastID > 0){
-			
+		if (lastID > 0) {
+
 			Etape etape = new Etape();
-			
+
 			etape.setEtp_id(stepID);
-			
-			result = tmesSpecService.getTemperatureMeasurementsAfterID(etape, uuid, lastID, delayInMinutes);
+
+			result = tmesSpecService.getTemperatureMeasurementsAfterID(etape,
+					uuid, lastID, delayInMinutes);
 			jsonResult = this.convertListToJSONObject(result);
-			
+
 		} else {
-			
+
 			logger.warning("You are trying to update with ID 0");
 		}
-		
 
 		return Response.status(200).entity(jsonResult.toString()).build();
 	}
-	
+
 	/**
-	 * Method used to return most recent measured temperature for step and eventually UUID.
+	 * Method used to return most recent measured temperature for step and
+	 * eventually UUID.
 	 * 
-	 *    If you want to get temperatures for all available 
+	 * If you want to get temperatures for all available
 	 * 
 	 * @param stepID
 	 * @param uuid
 	 * @return
+	 * @throws JSONException
 	 */
 	@GET
 	@Path("/lastTemperatureValue")
-	public Response getLastTemperatureValue (@PathParam("e") long stepID, @PathParam("u") String uuid){
-		
-		List<TemperatureMeasurement> response = new ArrayList<TemperatureMeasurement> ();
+	public Response getLastTemperatureValue(@PathParam("e") long stepID,
+			@PathParam("u") String uuid) throws JSONException {
+
+		List<TemperatureMeasurement> response = new ArrayList<TemperatureMeasurement>();
 		JSONArray jsonResult = null;
 
-		if (stepID > 0 && uuid != null){
-			
-			response = tmesSpecService.getLastTemperatureByStepAndUUID(stepID, uuid);
-			
-			logger.fine("Got "+response.size()+" for UUID "+uuid+" and step "+stepID);
+		if (stepID > 0 && uuid != null) {
+
+			Etape step = stepService.getElementById(stepID);
+
+			response = tmesSpecService.getLastTemperatureByStepAndUUID(step,
+					uuid);
+
+			if (!uuid.equalsIgnoreCase("all") && response.size() == 1) {
+				logger.fine("Got " + response.size() + " for UUID " + uuid
+						+ " and step " + stepID);
+			}
 
 		}
-		
+
 		jsonResult = this.convertListToJSONObject(response);
 
-		
 		return Response.status(200).entity(jsonResult.toString()).build();
 	}
 
@@ -206,6 +221,13 @@ public class RESTTemperatureService {
 		return json;
 	}
 
+	/**
+	 * Puts object attributes in JSON object
+	 * 
+	 * @param toConvert
+	 * @return
+	 * @throws JSONException
+	 */
 	JSONObject convertToJSONObject(TemperatureMeasurement toConvert)
 			throws JSONException {
 		JSONObject json = null;
